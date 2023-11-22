c187ac54a09b5b020d15252b55b5b3daa7b765f7
Okwudili-Daniel <144442872+Okwudili-Daniel@users.noreply.github.com>
2023-11-22 11:17:19 +0100

    Daniel

--- index.ts
+++ index.ts
@@ -15,3 +15,10 @@ process.on("uncaughtException", (err: Error) =>{
     console.log("uncaughtException", err)
     process.exit(1);
 })
+
+process.on("unhandledRejection", (reason) =>{
+    console.log("unhandledRejection", reason)
+    server.close(() =>{
+        process.exit(1);
+    })
+})
